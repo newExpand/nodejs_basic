0027950fe48d76cbc07c519c829e5e51629ad8a0
newExpand <magin0auto@gmail.com>
2022-12-18 13:10:12 +0900

    로그인을 요청한ㄴ fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -7,8 +7,16 @@ const id = document.querySelector("#id"),
 loginBtn.addEventListener("click", login);
 
 function login() {
-    const req = {
-        id : id.value,
-        psword: psword.value
-    }
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+
+  fetch("/login", {
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(req),
+  });
 }
